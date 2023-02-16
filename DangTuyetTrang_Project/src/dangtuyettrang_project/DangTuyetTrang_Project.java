181067934ae6a4a5831281f3b38b99948423f26c
DangTuyetTrang <125582667+DangTuyetTrang@users.noreply.github.com>
2023-02-16 23:48:57 +0700

    Update DangTuyetTrang_Project.java

--- DangTuyetTrang_Project/src/dangtuyettrang_project/DangTuyetTrang_Project.java
+++ DangTuyetTrang_Project/src/dangtuyettrang_project/DangTuyetTrang_Project.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+// Cô ơi máy e nó bị lỗi nên e k đổi đc tên file ạ
 package dangtuyettrang_project;
 
-/**
- *
- * @author Đặng Tuyết Trang
- */
 public class DangTuyetTrang_Project {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Đặng Tuyết Trang học Java");
-    }
+        System.out.println("Đặng Tuyết Trang học Java");     
+    } 
 
 }
