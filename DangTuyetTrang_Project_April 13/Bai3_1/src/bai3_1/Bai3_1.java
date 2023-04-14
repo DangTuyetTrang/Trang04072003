581f101fc701e89c7a80dcc038ee45960765ec77
DangTuyetTrang <tuyettrangdang0207humg@gmail.com>
2023-04-14 10:18:17 +0700

    4th source

---
+++ DangTuyetTrang_Project_April 13/Bai3_1/src/bai3_1/Bai3_1.java
@@ -0,0 +1,18 @@
+package bai3_1;
+
+import data.sach;
+
+public class Bai3_1 {
+
+    
+    public static void main(String[] args) {
+        sach Sach = new sach();
+        
+        System.out.println("Nhập thông tin cuốn sách");
+        Sach.nhapSach();
+        
+        System.out.println("\nThông tin cuốn sách");
+        Sach.inSach();
+    }
+    
+}
